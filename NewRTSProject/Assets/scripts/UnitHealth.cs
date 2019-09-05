6bcb22a6bb6c05ef179cc318a14620f2a2a10337
GarrettYin <garrett.yin@gmail.com>
2019-09-05 18:20:13 -0500

    start creation of the RTS game

---
+++ NewRTSProject/Assets/scripts/UnitHealth.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitHealth : MonoBehaviour
+{
+    public float Health;
+    public float maxHealth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
